1fe2aa5dfaca324c4c08e4ab485801e994e8d488
Carlos <cfernandez@worldsensing.com>
2024-08-16 18:06:37 +0200

    Actualizamos TDK_Test.py

--- TDK_Test.py
+++ TDK_Test.py
@@ -29,100 +29,253 @@ import subprocess
 import serial
 import time
 from colorama import Fore, Style, Back
+#from BK_param import Parametriza_BK
 
 FTDI = '/dev/ttyUSB0'
-BK='/dev/ttyUSB1'
+BK='/dev/ttyUSB4'
 
 
 # Preparar MSP para consulta presion
-TDK_DP1 = ["l1"]  
+TDK_DP1 = ["l1","l2","l3"]  
+TDK_DP2 = ["m1","m2","m3"] 
+TDK_DP3 = ["n1","n2","n3"] 
 
 def leer_tdk():
     respuesta =""
-    # Abrir serial para mandar command leer presión
+    # Abrir serial para mandar command parametrización de voltaje en TDK
     ser = serial.Serial(FTDI, 115200)  # Reemplaza '/dev/ttyFTDI' con el puerto correcto
     ser.timeout = 1  # Tiempo de espera para la lectura en segundos
+
 
     for comando in TDK_DP1:
         ser.write(comando.encode())
         #print(f"Ejecutando comando: {comando}")
-        time.sleep(0.4)
+        
+        # Abrir serial1 para mandar command a la carga electronica BK8600
+        ser1 = serial.Serial(BK, 9600,bytesize=serial.EIGHTBITS, 
+                            parity=serial.PARITY_NONE, 
+                            stopbits=serial.STOPBITS_ONE, 
+                            timeout=1)  # Parametrizamos la comunicación serie BK
+        
+        try:
+            # Mandamos la orden de lectura a la carga electronica BK8600
+            ser1.write(b':REM:SENS 1\n')
+            time.sleep(0.5)
+
+            # Configuramos a 1.50 Amp la carga
+            ser1.write(b'SOUR:CURR 1.50\n')
+            time.sleep(0.5)
+            
+            # Mandamos la orden de control remoto a la carga electronica BK8600
+            ser1.write(b':SYST:REM\n')
+            time.sleep(0.5)
+
+            # Pedimos la versión del software de la carga electronica
+            ser1.write(b':SYST:VERS?\n')
+            time.sleep(0.2)
+            Ver = ser1.readline().decode().strip()
+
+            # Pedimos la identidad de la carga electronica
+            ser1.write(b'*IDN?\n')
+            time.sleep(0.2)
+            Instrument = ser1.readline().decode().strip()
+
+            # Mandamos la orden de ON de la carga electronica BK8600
+            ser1.write(b'SOUR:INP:STAT 1\n')
+            time.sleep(2)
+            
+
+            #Mandamos la petición de lectura de voltaje y corriente   
+            ser1.write(b'REM:SENS 1;:MEAS:VOLT?;CURR?\n')
+            time.sleep(0.3)
+            
+            # Mandamos la orden de OFF de la carga electronica BK8600
+            ser1.write(b'SOUR:INP:STAT 0\n')
+            time.sleep(0.5)
+
+            #leemos la respuesta
+            voltaje = ser1.readline().decode().strip().split(";")
+            print(f'{"Test realizado con: "}{Fore.YELLOW}{Instrument}{Style.RESET_ALL}')
+            print(f'{"Version de la BK8600: "}{Fore.YELLOW}{Ver}{Style.RESET_ALL}')
+            print(f'{"Voltaje leido --> "}{Fore.YELLOW}{voltaje[0]+" Volt"}{Style.RESET_ALL}')
+            print(f'{"Corriente leida --> "}{Fore.YELLOW}{voltaje[1]+" Amp"}{Style.RESET_ALL}')
+            Volt=voltaje[0]
+            Amp=voltaje[1]
+            if float(Volt) > 11.0:
+                print(f'{Fore.GREEN}{"Test de Voltaje --> PASS"}{Style.RESET_ALL}')
+            else:
+                print(f'{Fore.RED}{"Test de Voltaje --> FAIL"}{Style.RESET_ALL}')
+            
+            if float(Amp) > 1.48:
+                print(f'{Fore.GREEN}{"Test de Corriente --> PASS"}{Style.RESET_ALL}')
+            else:
+                print(f'{Fore.RED}{"Test de Corriente --> FAIL"}{Style.RESET_ALL}')
+            
+    
+
+        except Exception as e:
+            print(f'Error al leer en BK: {e}')
+        finally:
+            ser1.close()
+            time.sleep(0.4)
+    
+    print(f'Conecta el cable de la BK en el puerto DP2')
+    hola = input('pulsa Enter para iniciar el test')
+
+    for comando in TDK_DP2:
+        ser.write(comando.encode())
+        #print(f"Ejecutando comando: {comando}")
+        # Abrir serial1 para mandar command a la carga electronica BK8600
+        ser1 = serial.Serial(BK, 9600,bytesize=serial.EIGHTBITS, 
+                            parity=serial.PARITY_NONE, 
+                            stopbits=serial.STOPBITS_ONE, 
+                            timeout=1)  # Parametrizamos la comunicación serie BK
+        
+        try:
+            # Mandamos la orden de lectura a la carga electronica BK8600
+            ser1.write(b':REM:SENS 1\n')
+            time.sleep(0.5)
+
+            # Configuramos a 1.50 Amp la carga
+            ser1.write(b'SOUR:CURR 1.50\n')
+            time.sleep(0.5)
+            
+            # Mandamos la orden de control remoto a la carga electronica BK8600
+            ser1.write(b':SYST:REM\n')
+            time.sleep(0.5)
+
+            # Pedimos la versión del software de la carga electronica
+            ser1.write(b':SYST:VERS?\n')
+            time.sleep(0.2)
+            Ver = ser1.readline().decode().strip()
+
+            # Pedimos la identidad de la carga electronica
+            ser1.write(b'*IDN?\n')
+            time.sleep(0.2)
+            Instrument = ser1.readline().decode().strip()
+
+            # Mandamos la orden de ON de la carga electronica BK8600
+            ser1.write(b'SOUR:INP:STAT 1\n')
+            time.sleep(2)
+            
+
+            #Mandamos la petición de lectura de voltaje y corriente   
+            ser1.write(b'REM:SENS 1;:MEAS:VOLT?;CURR?\n')
+            time.sleep(0.3)
+            
+            # Mandamos la orden de OFF de la carga electronica BK8600
+            ser1.write(b'SOUR:INP:STAT 0\n')
+            time.sleep(0.5)
+
+            #leemos la respuesta
+            voltaje = ser1.readline().decode().strip().split(";")
+            print(f'{"Test realizado con: "}{Fore.YELLOW}{Instrument}{Style.RESET_ALL}')
+            print(f'{"Version de la BK8600: "}{Fore.YELLOW}{Ver}{Style.RESET_ALL}')
+            print(f'{"Voltaje leido --> "}{Fore.YELLOW}{voltaje[0]+" Volt"}{Style.RESET_ALL}')
+            print(f'{"Corriente leida --> "}{Fore.YELLOW}{voltaje[1]+" Amp"}{Style.RESET_ALL}')
+            Volt=voltaje[0]
+            Amp=voltaje[1]
+            if float(Volt) > 11.0:
+                print(f'{Fore.GREEN}{"Test de Voltaje --> PASS"}{Style.RESET_ALL}')
+            else:
+                print(f'{Fore.RED}{"Test de Voltaje --> FAIL"}{Style.RESET_ALL}')
+            
+            if float(Amp) > 1.48:
+                print(f'{Fore.GREEN}{"Test de Corriente --> PASS"}{Style.RESET_ALL}')
+            else:
+                print(f'{Fore.RED}{"Test de Corriente --> FAIL"}{Style.RESET_ALL}')
+            
+    
+
+        except Exception as e:
+            print(f'Error al leer en BK: {e}')
+        finally:
+            ser1.close()
+            time.sleep(0.4)
+    print(f'Conecta el cable de la BK en el puerto DP3')
+    hola = input('pulsa Enter para iniciar el test')
+
+    for comando in TDK_DP3:
+        ser.write(comando.encode())
+        #print(f"Ejecutando comando: {comando}")
+        # Abrir serial1 para mandar command a la carga electronica BK8600
+        ser1 = serial.Serial(BK, 9600,bytesize=serial.EIGHTBITS, 
+                            parity=serial.PARITY_NONE, 
+                            stopbits=serial.STOPBITS_ONE, 
+                            timeout=1)  # Parametrizamos la comunicación serie BK
+        
+        try:
+            # Mandamos la orden de lectura a la carga electronica BK8600
+            ser1.write(b':REM:SENS 1\n')
+            time.sleep(0.5)
+
+            # Configuramos a 1.50 Amp la carga
+            ser1.write(b'SOUR:CURR 1.50\n')
+            time.sleep(0.5)
+            
+            # Mandamos la orden de control remoto a la carga electronica BK8600
+            ser1.write(b':SYST:REM\n')
+            time.sleep(0.5)
+
+            # Pedimos la versión del software de la carga electronica
+            ser1.write(b':SYST:VERS?\n')
+            time.sleep(0.2)
+            Ver = ser1.readline().decode().strip()
+
+            # Pedimos la identidad de la carga electronica
+            ser1.write(b'*IDN?\n')
+            time.sleep(0.2)
+            Instrument = ser1.readline().decode().strip()
+
+            # Mandamos la orden de ON de la carga electronica BK8600
+            ser1.write(b'SOUR:INP:STAT 1\n')
+            time.sleep(2)
+            
+
+            #Mandamos la petición de lectura de voltaje y corriente   
+            ser1.write(b'REM:SENS 1;:MEAS:VOLT?;CURR?\n')
+            time.sleep(0.3)
+            
+            # Mandamos la orden de OFF de la carga electronica BK8600
+            ser1.write(b'SOUR:INP:STAT 0\n')
+            time.sleep(0.5)
+
+            #leemos la respuesta
+            voltaje = ser1.readline().decode().strip().split(";")
+            print(f'{"Test realizado con: "}{Fore.YELLOW}{Instrument}{Style.RESET_ALL}')
+            print(f'{"Version de la BK8600: "}{Fore.YELLOW}{Ver}{Style.RESET_ALL}')
+            print(f'{"Voltaje leido --> "}{Fore.YELLOW}{voltaje[0]+" Volt"}{Style.RESET_ALL}')
+            print(f'{"Corriente leida --> "}{Fore.YELLOW}{voltaje[1]+" Amp"}{Style.RESET_ALL}')
+            Volt=voltaje[0]
+            Amp=voltaje[1]
+            if float(Volt) > 11.0:
+                print(f'{Fore.GREEN}{"Test de Voltaje --> PASS"}{Style.RESET_ALL}')
+            else:
+                print(f'{Fore.RED}{"Test de Voltaje --> FAIL"}{Style.RESET_ALL}')
+            
+            if float(Amp) > 1.48:
+                print(f'{Fore.GREEN}{"Test de Corriente --> PASS"}{Style.RESET_ALL}')
+            else:
+                print(f'{Fore.RED}{"Test de Corriente --> FAIL"}{Style.RESET_ALL}')
+            
+    
+
+        except Exception as e:
+            print(f'Error al leer en BK: {e}')
+        finally:
+            ser1.close()
+            time.sleep(0.4)
     ser.close()
 
-    # Abrir serial1 para mandar command a la carga electronica BK8600
-    ser1 = serial.Serial(BK, 9600,bytesize=serial.EIGHTBITS, 
-                         parity=serial.PARITY_NONE, 
-                         stopbits=serial.STOPBITS_ONE, 
-                         timeout=1)  # Parametrizamos la comunicación serie
     
-    #hola = input('pulsa ON en la fuente BK para verificar la corriente')
-    try:
-        # Mandamos la orden de lectura a la carga electronica BK8600
-        ser1.write(b'REM:SENS 1\n')
-        time.sleep(0.5)
-
-        # Configuramos a 1.50 Amp la carga
-        ser1.write(b'SOUR:CURR 1.50\n')
-        time.sleep(0.5)
-        
-        # Mandamos la orden de control remoto a la carga electronica BK8600
-        ser1.write(b'SYST:REM\n')
-        time.sleep(0.5)
-
-        # Pedimos la versión del software de la carga electronica
-        ser1.write(b':SYST:VERS?\n')
-        time.sleep(0.2)
-        Ver = ser1.readline().decode().strip()
-
-        # Pedimos la identidad de la carga electronica
-        ser1.write(b'*IDN?\n')
-        time.sleep(0.2)
-        Instrument = ser1.readline().decode().strip()
-
-        # Mandamos la orden de ON de la carga electronica BK8600
-        ser1.write(b'SOUR:INP:STAT 1\n')
-        time.sleep(2)
-        
-
-        #Mandamos la petición de lectura de voltaje y corriente   
-        ser1.write(b'REM:SENS 1;:MEAS:VOLT?;CURR?\n')
-        time.sleep(0.3)
-        
-        # Mandamos la orden de OFF de la carga electronica BK8600
-        ser1.write(b'SOUR:INP:STAT 0\n')
-        time.sleep(0.5)
-
-        #leemos la respuesta
-        voltaje = ser1.readline().decode().strip().split(";")
-        print(f'{"Test realizado con: "}{Fore.YELLOW}{Instrument}{Style.RESET_ALL}')
-        print(f'{"Version de la BK8600: "}{Fore.YELLOW}{Ver}{Style.RESET_ALL}')
-        print(f'{"Voltaje leido --> "}{Fore.YELLOW}{voltaje[0]+" Volt"}{Style.RESET_ALL}')
-        print(f'{"Corriente leida --> "}{Fore.YELLOW}{voltaje[1]+" Amp"}{Style.RESET_ALL}')
-        Volt=voltaje[0]
-        Amp=voltaje[1]
-        if float(Volt) > 11.0:
-            print(f'{Fore.GREEN}{"Test de Voltaje --> PASS"}{Style.RESET_ALL}')
-        else:
-            print(f'{Fore.RED}{"Test de Voltaje --> FAIL"}{Style.RESET_ALL}')
-        
-        if float(Amp) > 1.48:
-            print(f'{Fore.GREEN}{"Test de Corriente --> PASS"}{Style.RESET_ALL}')
-        else:
-            print(f'{Fore.RED}{"Test de Corriente --> FAIL"}{Style.RESET_ALL}')
-        
-   
-
-    except Exception as e:
-        print(f'Error al leer en BK: {e}')
-    finally:
-        ser1.close()
     
-    # Cierra el puerto DP1
-    cierre_DP1 ="G1"
+    # Apaga los puertos DP
+    cierre_DP =["G1", "G2", "G3"]
     ser = serial.Serial(FTDI, 115200)  
     ser.timeout = 1  # Tiempo de espera para la lectura en segundos
-    ser.write(cierre_DP1.encode())
-    time.sleep(0.7)
+    for comando in cierre_DP:
+        ser.write(comando.encode())
+        time.sleep(0.7)
     ser.close()
 
 

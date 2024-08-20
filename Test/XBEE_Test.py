f8382929b948b817b5b06541c4bd0cac253899de
Carlos <cfernandez@worldsensing.com>
2024-08-20 18:04:25 +0200

    actualización XBee

--- Test/XBEE_Test.py
+++ Test/XBEE_Test.py
@@ -19,7 +19,9 @@ Press +++ to enter command mode (no enter key)
 The module should reply ‘OK’
 Enter AT <enter>
 The module should reply ‘OK’
-If you do not get a response from module, probe the Reset pin (pin 5 on J6 left side of XBee). It should be high. If it is low then enter the command to enable Xbee reset. Then probe both UART lines. Both should be high. If they were being pulled low then something may be not functional. 
+If you do not get a response from module, probe the Reset pin (pin 5 on J6 left side of XBee). 
+It should be high. If it is low then enter the command to enable Xbee reset. Then probe both UART lines. 
+Both should be high. If they were being pulled low then something may be not functional. 
 
 To rule out the module:
 Power off the board 
@@ -29,7 +31,8 @@ Power on the board, and enable the Octavo (command ‘E2’)
 Open screen and connect to TTY04:
 screen /dev/ttyO4 9600
 Enter text into the screen window. It should be echoed back.
-If this is not working then there may be an issue with the Octavo OR the UART multiplexer on UART4 may be set to a different destination, e.g. Expansion Port.
+If this is not working then there may be an issue with the Octavo OR the UART multiplexer on UART4 
+may be set to a different destination, e.g. Expansion Port.
 
 **********************************
 Testing
